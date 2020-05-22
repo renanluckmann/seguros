60b2ce06c919869f21a9292e9f0b676da6c8e17b
rluckmann <rluckmann@DESKTOP-023RENV>
2020-05-22 15:04:22 -0300

    updates to deploy to heroku

--- src/app/server.js
+++ src/app/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/seguro-app'));
+app.use(express.static('./dist/seguros'));
 
 app.get('/*', function(req,res) {
-    res.sendFile('index.html', {root: 'dist/seguro-app/'});
+    res.sendFile('index.html', {root: 'dist/seguros/'});
 });
 
 // Start the app by listening on the default Heroku port
